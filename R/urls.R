2770b119178de92b296dc65ec1beb98edc4147d9
John Wilshire <john.h.wilshire@gmail.com>
2021-02-26 13:05:53 -0500

    fail gracefully cran req, WIP

--- R/urls.R
+++ R/urls.R
@@ -15,6 +15,7 @@ build_url <- function(..., otf = FALSE) {
 ua <- function () httr::user_agent(paste("rstoat", utils::packageVersion("rstoat")))
 
 http_error_handle_parse <- function(resp, enc = "UTF-8", simplifyVector = TRUE) {
+  # see: https://community.rstudio.com/t/internet-resources-should-fail-gracefully/49199/7
   if (httr::http_error(resp)) {
     e_message <- tryCatch(
     {
@@ -27,30 +28,58 @@ http_error_handle_parse <- function(resp, enc = "UTF-8", simplifyVector = TRUE) 
       "STOAT API request failed [", httr::status_code(resp),"]\n",
       e_message
     )
-    stop(error_message, call. = F)
+    message(error_message, call. = F)
+    return(NULL)
   }
   if (httr::http_type(resp) != "application/json") {
     message(httr::content(resp))
-    stop("API did not return json.", call. = FALSE)
+    message("API did not return json.", call. = FALSE)
+    return (NULL)
   }
   jsonlite::fromJSON(httr::content(resp, "text", encoding = enc), simplifyVector = simplifyVector)
 }
 
-get_json <- function (..., enc = "UTF-8", simplifyVector = TRUE, query = NULL, authenticate=TRUE) {
-  if (authenticate) {
-    resp <- httr::GET(build_url(...), ua(), get_auth_header(), query = query)
+err_func <- function(e) {
+  if(!curl::has_internet()) {
+    message('The rstoat package requires an internet connection, please connect to the internet.')
+    return(NULL)
   } else {
-    resp <- httr::GET(build_url(...), ua(), query = query)
+    message(paste('Could not connect to: ', base(), ', please raise an issue on github: https://github.com/MapofLife/rstoat/ .'))
+    return(NULL)
   }
+}
+
+get_resp <- function (block) {
+  tryCatch(
+    block,
+    error = err_func
+  )
+}
+
+get_json <- function (..., enc = "UTF-8", simplifyVector = TRUE, query = NULL, authenticate=TRUE) {
+  resp <- get_resp(
+    {
+      if (authenticate) {
+        resp <- httr::GET(build_url(...), ua(), get_auth_header(), query = query)
+      } else {
+        resp <- httr::GET(build_url(...), ua(), query = query)
+      }
+      resp
+    })
+  if (is.null(resp)) return(resp)
   http_error_handle_parse(resp, enc, simplifyVector = simplifyVector)
 }
 
 post_json <- function (..., body = list(), enc = "UTF-8", simplifyVector = TRUE, authenticate=TRUE) {
-  if (authenticate) {
-    resp <- httr::POST(build_url(...), body = body, encode = "json", ua(), get_auth_header())
-  } else {
-    resp <- httr::POST(build_url(...), body = body, encode = "json", ua())
-  }
+  resp <- get_resp({
+    if (authenticate) {
+      resp <- httr::POST(build_url(...), body = body, encode = "json", ua(), get_auth_header())
+    } else {
+      resp <- httr::POST(build_url(...), body = body, encode = "json", ua())
+    }
+    resp
+  })
+  if (is.null(resp)) return(resp)
   http_error_handle_parse(resp, enc, simplifyVector = simplifyVector)
 }
 
@@ -70,25 +99,28 @@ download_sample_data <- function(dir = 'sample_data') {
     dir.create(dir)
     message(paste0('Created directory: ', dir))
   }
+  res <- tryCatch({
+      message('Downloading powerful owl occurrence data.')
+      utils::download.file(build_url('samples/powerful_owl/vignette', otf=T), file.path(dir, 'powerful_owl_vignette.csv'), mode='wb')
 
-  message('Downloading powerful owl occurrence data.')
-  utils::download.file(build_url('samples/powerful_owl/vignette', otf=T), file.path(dir, 'powerful_owl_vignette.csv'), mode='wb')
+      message('Downloading budgerigar occurrence data.')
+      utils::download.file(build_url('samples/budgerigar/vignette', otf=T), file.path(dir, 'budgerigar_vignette.csv'), mode='wb')
 
-  message('Downloading budgerigar occurrence data.')
-  utils::download.file(build_url('samples/budgerigar/vignette', otf=T), file.path(dir, 'budgerigar_vignette.csv'), mode='wb')
+      tmp_path <- tempfile()
+      message('Downloading powerful owl annotated results data.')
+      utils::download.file(build_url('samples/powerful_owl/results', otf=T), file.path(tmp_path), mode='wb')
+      message('Unzipping powerful owl\n')
+      utils::unzip(tmp_path, exdir=paste0(dir, '/powerful_owl_results'))
 
-  tmp_path <- tempfile()
-  message('Downloading powerful owl annotated results data.')
-  utils::download.file(build_url('samples/powerful_owl/results', otf=T), file.path(tmp_path), mode='wb')
-  message('Unzipping powerful owl\n')
-  utils::unzip(tmp_path, exdir=paste0(dir, '/powerful_owl_results'))
+      message('Downloading budgerigar annotated results data.')
+      utils::download.file(build_url('samples/budgerigar/results', otf=T), file.path(tmp_path), mode='wb')
+      message('Unzipping budgerigar\n')
+      utils::unzip(tmp_path, exdir=paste0(dir, '/budgerigar_results'))
 
-  message('Downloading budgerigar annotated results data.')
-  utils::download.file(build_url('samples/budgerigar/results', otf=T), file.path(tmp_path), mode='wb')
-  message('Unzipping budgerigar\n')
-  utils::unzip(tmp_path, exdir=paste0(dir, '/budgerigar_results'))
-
-  message(paste0('Annotation available in directory: ', dir))
-  return(dir)
+      message(paste0('Annotation available in directory: ', dir))
+      dir
+  }, error = err_func, warning = err_func
+  )
+  return(res)
 }
 
